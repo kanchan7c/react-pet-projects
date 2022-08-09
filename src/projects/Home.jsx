05ca613bd56872703e316458c8b81be919f2506b
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2022-08-09 21:58:55 +0530

    installed Tailwind CSS and changed the styling using tailwind css classes

--- src/projects/Home.jsx
+++ src/projects/Home.jsx
@@ -4,8 +4,49 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <ul>
-        <Link to='/todo'>TODO App</Link>
+      <ul className='grid lg:grid-cols-5 sm:grid-cols-3 gap-1'>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
+        <Link
+          className='border border-gray-800 bg-gray-700 hover:scale-95 text-gray-50 px-2 py-1 text-center rounded transition-transform'
+          to='/todo'
+        >
+          TODO App
+        </Link>
       </ul>
     </div>
   );
